72e9536ac4b21d3eedff5af2d7a0e5950787f905
Kate <iseeyou926@gmail.com>
2016-09-13 07:58:55 +0300

    first test commit

---
+++ src/main/java/servlet/Servlet.java
@@ -0,0 +1,30 @@
+package java.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Kate on 13.09.2016.
+ */
+@WebServlet("/web")
+public class Servlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        super.doPost(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        out.println("Test Text on My Web");
+    }
+}
